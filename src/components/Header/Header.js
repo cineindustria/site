7a906436839771006f673c5881685f294b6a28c9
Nicolás Silva <sovietfrance@gmail.com>
2017-05-14 00:39:37 -0400

    feat: Update layout

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -12,8 +12,7 @@ import withStyles from 'isomorphic-style-loader/lib/withStyles';
 import s from './Header.css';
 import Link from '../Link';
 import Navigation from '../Navigation';
-import logoUrl from './logo-small.png';
-import logoUrl2x from './logo-small@2x.png';
+import logoUrl from './cinelogo.svg';
 
 class Header extends React.Component {
   render() {
@@ -22,12 +21,12 @@ class Header extends React.Component {
         <div className={s.container}>
           <Navigation />
           <Link className={s.brand} to="/">
-            <img src={logoUrl} srcSet={`${logoUrl2x} 2x`} width="38" height="38" alt="React" />
-            <span className={s.brandTxt}>Your Company</span>
+            <img src={logoUrl} width="38" height="38" alt="cineindustria.com" />
+            <span className={s.brandTxt}>cineindustria.com</span>
           </Link>
           <div className={s.banner}>
-            <h1 className={s.bannerTitle}>React</h1>
-            <p className={s.bannerDesc}>Complex web apps made easy</p>
+            <h1 className={s.bannerTitle}>cineindustria</h1>
+            <p className={s.bannerDesc}>A set of data visualization pieces related to cinema industry</p>
           </div>
         </div>
       </div>
